89110a19c348b9ba498922cb9ceb56fc8e7269e6
hyukzz <ri_chard@naver.com>
2023-04-04 00:42:41 +0900

    Feat: 로그인 페이지 구현

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,28 @@
+import { useLoginForm } from '../hooks/useSignInForm';
+import { useAuth } from '../hooks/useAuth';
+
 const SignIn = () => {
-  return <div>로그인</div>;
+  const { loginSubmitHandler } = useLoginForm();
+  useAuth();
+
+  return (
+    <div>
+      <h1>로그인 페이지</h1>
+      <form onSubmit={loginSubmitHandler}>
+        <label>
+          이메일:
+          <input type='email' name='email' data-testid='email-input' />
+        </label>
+        <label>
+          비밀번호:
+          <input type='password' name='password' data-testid='password-input' />
+        </label>
+        <button type='submit' value='Submit' data-testid='signin-button'>
+          로그인
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;

f8b951445fe5ae43fba8deceed2ed99aa37c70f3
hyukzz <ri_chard@naver.com>
2023-04-05 21:48:47 +0900

    Feat: axios 인스턴스 구현

---
+++ src/api/api.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use(requestConfig => {
+  const accessToken = localStorage.getItem('access_token');
+
+  if (accessToken && requestConfig.headers) {
+    requestConfig.headers['Authorization'] = `Bearer ${accessToken}`;
+  }
+
+  return requestConfig;
+});

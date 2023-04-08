77c845fbdfc918be4148b78486a44c41a10cd4bb
hyukzz <ri_chard@naver.com>
2023-04-09 03:57:32 +0900

    Feat: 로그아웃 모달 구현

--- src/components/SignOut.tsx
+++ src/components/SignOut.tsx
@@ -1,23 +1,55 @@
+import { useState } from 'react';
 import { useRouter } from '../hooks/useRouter';
-import SignOut_Icon from '../assets/signout_icon.png';
+import SignOutIcon from '../assets/signout_icon.png';
 
 const SignOut = () => {
   const { routeTo } = useRouter();
 
-  const handleSignOut = () => {
-    if (!window.confirm('로그아웃하시겠습니까?')) return;
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
+  const handleSignOut = () => {
+    setShowConfirmation(true);
+  };
+
+  const handleConfirmSignOut = () => {
     localStorage.removeItem('access_token');
     routeTo('/signin');
   };
 
+  const handleCancelSignOut = () => {
+    setShowConfirmation(false);
+  };
+
   return (
-    <button
-      className='inline-flex items-center px-4 py-2 border text-base font-medium rounded-md text-black bg-white hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 border-white'
-      onClick={handleSignOut}
-    >
-      <img src={SignOut_Icon} alt='Sign Out' className='w-5 h-5 mr-2' />
-    </button>
+    <>
+      <button
+        className='flex items-center justify-center space-x-2 px-4 py-2 rounded-md text-white bg-gradient-to-r from-blue-500 to-blue-700 hover:opacity-90 focus:outline-none'
+        onClick={handleSignOut}
+      >
+        <img src={SignOutIcon} alt='Sign Out' className='w-5 h-5 select-none' />
+      </button>
+      {showConfirmation && (
+        <div className='fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex items-center justify-center z-10'>
+          <div className='bg-white rounded-lg p-5 shadow-lg'>
+            <h2 className='text-lg font-medium mb-4 select-none'>로그아웃하시겠습니까?</h2>
+            <div className='flex justify-end'>
+              <button
+                className='bg-gray-400 hover:bg-gray-500 text-white font-medium rounded-md px-3 py-2 select-none mr-2'
+                onClick={handleCancelSignOut}
+              >
+                취소
+              </button>
+              <button
+                className='bg-red-500 hover:bg-red-600 text-white font-medium rounded-md px-3 py-2 select-none'
+                onClick={handleConfirmSignOut}
+              >
+                로그아웃
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
